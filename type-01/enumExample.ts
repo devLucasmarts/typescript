d79ac57ca24a7f19068fc2fa9feecf0a820b9dfe
martsDev <martssilvapro@gmail.com>
2022-08-04 11:37:47 -0300

    add enum example

--- type-01/enumExample.ts
+++ type-01/enumExample.ts
@@ -17,3 +17,20 @@ enum courseStatus {
 
 let newCourseStatus: courseStatus = courseStatus.Active; // referenciamos um enum usando EnumName.Value;
 console.log(newCourseStatus)
+
+// ==================================
+
+enum StatusCodes {
+    OK = 200,
+    BadRequest = 400,
+    Unauthorized = 401,
+    PaymentRequired = 402,
+    Forbidden = 403,
+    NotFound = 404,
+};
+
+// Enum aceita acesso aos valores tanto pela chave quanto pelo valor
+
+console.log(StatusCodes.OK) // 200
+console.log(StatusCodes[400]) // BadRequest
+console.log(StatusCodes.BadRequest) // 400
