3b79237dc0e18c5f29aa69f7e00652137acde576
martsDev <martssilvapro@gmail.com>
2022-08-04 11:31:46 -0300

    add enum example

--- type-01/enumExample.ts
+++ type-01/enumExample.ts
@@ -1,5 +1,16 @@
+// enum courseStatus {
+//     Active,
+//     Inactive,
+//     Paused,
+// };
+
+// let newCourseStatus: courseStatus = courseStatus.Active; // referenciamos um enum usando EnumName.Value;
+// console.log(newCourseStatus)
+
+// Alterando a enum para refletir os resultados seguindo dados externos
+
 enum courseStatus {
-    Active,
+    Active = 1,
     Inactive,
     Paused,
 };
