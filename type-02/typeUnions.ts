46f4c5665467a3031a3b886e0ecd3c4d2ef188dd
martsDev <martssilvapro@gmail.com>
2022-08-08 16:26:29 -0300

    adicona exercicio

---
+++ type-02/typeUnions.ts
@@ -0,0 +1,14 @@
+function retornaCpf(cpf: number | string) {
+    console.log("Seu cpf é " + cpf);
+};
+
+retornaCpf(89787171016);
+retornaCpf("967.951.780-21");
+
+
+let rg: number | string = '';
+
+rg =  "21.928.377-1";
+
+rg = 147045745;
+
